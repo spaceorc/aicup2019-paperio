fa135e89849f0512dddef7433b7bc2b89fa3c15c
Иван Дашкевич <spaceorc@yandex.ru>
2019-07-21 16:20:53 +0500

    Fix sim

--- BrutalTester/Sim/Territory.cs
+++ BrutalTester/Sim/Territory.cs
@@ -45,138 +45,6 @@ namespace BrutalTester.Sim
             return captured;
         }
 
-        private List<V> CaptureVoidsBetweenLines(List<V> lines)
-        {
-            var captured = new List<V>();
-
-            for (int i = 0; i < lines.Count; i++)
-            {
-                foreach (var point in lines[i].GetNeighboring(Env.WIDTH))
-                {
-                    var endIndex = lines.IndexOf(point);
-                    if (endIndex >= 0)
-                    {
-                        if (endIndex - i >= 8)
-                        {
-                            var path = lines.GetRange(i, endIndex - i);
-                            captured.AddRange(DoCapture(path));
-                        }
-                    }
-                }
-            }
-
-            return captured;
-        }
-
-        private List<V> DoCapture(List<V> boundary)
-        {
-            var poligon_x_arr = boundary.Select(b => b.X).ToArray();
-            var poligon_y_arr = boundary.Select(b => b.Y).ToArray();
-
-            var captured = new List<V>();
-
-            var max_x = poligon_x_arr.Max();
-            var max_y = poligon_y_arr.Max();
-            var min_x = poligon_x_arr.Min();
-            var min_y = poligon_y_arr.Min();
-
-            var x = max_x;
-
-            while (x > min_x)
-            {
-                var y = max_y;
-                while (y > min_y)
-                {
-                    var v = new V(x, y);
-                    if (!Points.Contains(v) && InPolygon(x, y, poligon_x_arr, poligon_y_arr))
-                    {
-                        Points.Add(v);
-                        captured.Add(v);
-                    }
-
-                    y -= Env.WIDTH;
-                }
-
-                x -= Env.WIDTH;
-            }
-
-            return captured;
-        }
-
-        private bool InPolygon(int x, int y, int[] xp, int[] yp)
-        {
-            var c = false;
-            for (int i = 0; i < xp.Length; i++)
-            {
-                var prev = (i + xp.Length - 1) % xp.Length;
-                if (((yp[i] <= y && y < yp[prev]) || (yp[prev] <= y && y < yp[i])) &&
-                    (x > (xp[prev] - xp[i]) * (y - yp[i]) / (yp[prev] - yp[i]) + xp[i]))
-                    c = !c;
-            }
-
-            return c;
-        }
-
-        private List<List<V>> GetVoidsBetweenLinesAndTerritory(List<V> lines)
-        {
-            var boundary = GetBoundary();
-            var voids = new List<List<V>>();
-
-            foreach (var cur in lines)
-            {
-                foreach (var point in cur.GetNeighboring(Env.WIDTH))
-                {
-                    if (boundary.Contains(point))
-                    {
-                        var startPoint = GetNearestBoundary(lines[0], boundary);
-                        if (startPoint != null)
-                        {
-                            var endIndex = boundary.IndexOf(point);
-                            var startIndex = boundary.IndexOf(startPoint);
-                            var path = GetPath(startIndex, endIndex, boundary);
-                            if (path == null)
-                                continue;
-
-                            if (path.Count > 1 && path[0] == path[path.Count - 1])
-                                path = path.GetRange(1, path.Count - 1);
-
-                            voids.Add(
-                                lines
-                                    .GetRange(0, lines.IndexOf(cur) + 1)
-                                    .Concat(path.Select(index => boundary[index]))
-                                    .ToList()
-                            );
-                        }
-                    }
-                }
-            }
-
-            return voids;
-        }
-
-        private V GetNearestBoundary(V point, List<V> boundary)
-        {
-            foreach (var neighbor in point.GetSelfAndNeighboring(Env.WIDTH))
-            {
-                if (boundary.Contains(neighbor))
-                    return neighbor;
-            }
-
-            return null;
-        }
-
-        public List<V> GetBoundary()
-        {
-            var boundary = new List<V>();
-            foreach (var point in Points)
-            {
-                if (point.GetNeighboring(Env.WIDTH).Any(neighboring => !Points.Contains(neighboring)))
-                    boundary.Add(point);
-            }
-
-            return boundary;
-        }
-
         public List<V> RemovePoints(IEnumerable<V> points)
         {
             var removed = new List<V>();
@@ -242,10 +110,142 @@ namespace BrutalTester.Sim
             return removed;
         }
 
+        private List<V> GetBoundary()
+        {
+            var boundary = new List<V>();
+            foreach (var point in Points)
+            {
+                if (point.GetNeighboring(Env.WIDTH).Any(neighboring => !Points.Contains(neighboring)))
+                    boundary.Add(point);
+            }
+
+            return boundary;
+        }
+
+        private List<V> CaptureVoidsBetweenLines(List<V> lines)
+        {
+            var captured = new List<V>();
+
+            for (var i = 0; i < lines.Count; i++)
+            {
+                foreach (var point in lines[i].GetNeighboring(Env.WIDTH))
+                {
+                    var endIndex = lines.IndexOf(point);
+                    if (endIndex >= 0)
+                    {
+                        if (endIndex - i >= 8)
+                        {
+                            var path = lines.GetRange(i, endIndex - i);
+                            captured.AddRange(DoCapture(path));
+                        }
+                    }
+                }
+            }
+
+            return captured;
+        }
+
+        private List<V> DoCapture(List<V> boundary)
+        {
+            var poligon_x_arr = boundary.Select(b => b.X).ToArray();
+            var poligon_y_arr = boundary.Select(b => b.Y).ToArray();
+
+            var captured = new List<V>();
+
+            var max_x = poligon_x_arr.Max();
+            var max_y = poligon_y_arr.Max();
+            var min_x = poligon_x_arr.Min();
+            var min_y = poligon_y_arr.Min();
+
+            var x = max_x;
+
+            while (x > min_x)
+            {
+                var y = max_y;
+                while (y > min_y)
+                {
+                    var v = new V(x, y);
+                    if (!Points.Contains(v) && InPolygon(x, y, poligon_x_arr, poligon_y_arr))
+                    {
+                        Points.Add(v);
+                        captured.Add(v);
+                    }
+
+                    y -= Env.WIDTH;
+                }
+
+                x -= Env.WIDTH;
+            }
+
+            return captured;
+        }
+
+        private bool InPolygon(int x, int y, int[] xp, int[] yp)
+        {
+            var c = false;
+            for (var i = 0; i < xp.Length; i++)
+            {
+                var prev = (i + xp.Length - 1) % xp.Length;
+                if ((yp[i] <= y && y < yp[prev] || yp[prev] <= y && y < yp[i]) &&
+                    x > (xp[prev] - xp[i]) * (y - yp[i]) / (yp[prev] - yp[i]) + xp[i])
+                    c = !c;
+            }
+
+            return c;
+        }
+
+        private List<List<V>> GetVoidsBetweenLinesAndTerritory(List<V> lines)
+        {
+            var boundary = GetBoundary();
+            var voids = new List<List<V>>();
+
+            foreach (var cur in lines)
+            {
+                foreach (var point in cur.GetNeighboring(Env.WIDTH))
+                {
+                    if (boundary.Contains(point))
+                    {
+                        var startPoint = GetNearestBoundary(lines[0], boundary);
+                        if (startPoint != null)
+                        {
+                            var endIndex = boundary.IndexOf(point);
+                            var startIndex = boundary.IndexOf(startPoint);
+                            var path = GetPath(startIndex, endIndex, boundary);
+                            if (path == null)
+                                continue;
+
+                            if (path.Count > 1 && path[0] == path[path.Count - 1])
+                                path = path.GetRange(1, path.Count - 1);
+
+                            voids.Add(
+                                lines
+                                    .GetRange(0, lines.IndexOf(cur) + 1)
+                                    .Concat(path.Select(index => boundary[index]))
+                                    .ToList()
+                            );
+                        }
+                    }
+                }
+            }
+
+            return voids;
+        }
+
+        private V GetNearestBoundary(V point, List<V> boundary)
+        {
+            foreach (var neighbor in point.GetSelfAndNeighboring(Env.WIDTH))
+            {
+                if (boundary.Contains(neighbor))
+                    return neighbor;
+            }
+
+            return null;
+        }
+
         private List<int> GetPath(int startIndex, int endIndex, List<V> boundary)
         {
             var graph = new Graph();
-            for (int index = 0; index < boundary.Count; index++)
+            for (var index = 0; index < boundary.Count; index++)
             {
                 var point = boundary[index];
                 var siblings = point.GetVertAndHoriz(Env.WIDTH).Where(boundary.Contains);
