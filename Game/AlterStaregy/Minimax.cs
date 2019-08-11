f911987f4abae40f394754196a874d3f303a8ef2
Иван Дашкевич <spaceorc@yandex.ru>
2019-08-11 22:24:14 +0500

    Fix minimax

--- Game/AlterStaregy/Minimax.cs
+++ Game/AlterStaregy/Minimax.cs
@@ -58,40 +58,10 @@ namespace Game.AlterStaregy
             if (timeManager.IsExpired)
                 return double.NegativeInfinity;
 
-            if (depth == 0 || state.isGameOver || state.players[player].status == PlayerStatus.Eliminated)
+            if (depth == 0 || state.isGameOver || state.players[player].status == PlayerStatus.Eliminated || activePlayer == state.players.Length)
             {
                 estimations++;
-                UndoData undo = null;
-                if (activePlayer != player && !state.isGameOver)
-                {
-                    for (int p = activePlayer; p < state.players.Length; p++)
-                    {
-                        if (p == player
-                            || state.players[p].arriveTime != 0
-                            || state.players[p].status == PlayerStatus.Eliminated
-                            || state.players[p].status == PlayerStatus.Broken)
-                            continue;
-
-                        for (byte d = 3; d <= 5; d++)
-                        {
-                            var action = (Direction)(((byte)state.players[p].dir.Value + d) % 4);
-                            var ne = state.NextCoord(state.players[p].arrivePos, action);
-                            if (ne != ushort.MaxValue)
-                            {
-                                commands[commandsStart + p] = action;
-                                if (state.territory[ne] == p)
-                                    break;
-                            }
-                        }
-                    }
-
-                    undo = state.NextTurn(commands, withUndo: true, commandsStart: commandsStart);
-                }
-
                 var score = estimator.Estimate(state, player);
-                if (undo != null)
-                    state.Undo(undo);
-
                 return score;
             }
 
@@ -100,13 +70,18 @@ namespace Game.AlterStaregy
             for (byte d = 3; d <= top; d++)
             {
                 var action = (Direction)(((byte)(state.players[activePlayer].dir ?? Direction.Up) + d) % 4);
+                var ne = state.NextCoord(state.players[activePlayer].arrivePos, action);
+                if (ne == ushort.MaxValue || (state.lines[ne] & (1 << activePlayer)) != 0)
+                    continue;
+                
                 commands[commandsStart + activePlayer] = action;
                 int nextPlayer = activePlayer == player ? 0 : activePlayer + 1;
                 for (; nextPlayer < state.players.Length; nextPlayer++)
                 {
                     if (nextPlayer == player
                         || state.players[nextPlayer].status == PlayerStatus.Eliminated
-                        || state.players[nextPlayer].status == PlayerStatus.Broken)
+                        || state.players[nextPlayer].status == PlayerStatus.Broken
+                        || state.players[nextPlayer].arriveTime > 0)
                         continue;
                     break;
                 }
@@ -115,7 +90,21 @@ namespace Game.AlterStaregy
                 if (nextPlayer == state.players.Length)
                 {
                     undo = state.NextTurn(commands, withUndo: true, commandsStart:commandsStart);
-                    nextPlayer = player;
+                    if (state.players[player].status != PlayerStatus.Eliminated && state.players[player].arriveTime == 0)
+                        nextPlayer = player;
+                    else
+                    {
+                        for (nextPlayer = 0; nextPlayer < state.players.Length; nextPlayer++)
+                        {
+                            if (nextPlayer == player
+                                || state.players[nextPlayer].status == PlayerStatus.Eliminated
+                                || state.players[nextPlayer].status == PlayerStatus.Broken
+                                || state.players[nextPlayer].arriveTime > 0)
+                                continue;
+                            break;
+                        }
+                    }
+
                     commandsStart += 6;
                 }
 
